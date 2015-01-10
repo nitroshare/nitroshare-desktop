e715209e36aae771835122e29cb0a81085707142
Nathan Osman <nathan@quickmediasolutions.com>
2015-01-10 14:35:58 -0800

    Added Monitor class.

---
+++ src/discovery/monitor.cpp
@@ -0,0 +1,53 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Nathan Osman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ **/
+
+#include <QNetworkInterface>
+
+#include "monitor.h"
+
+Monitor::Monitor()
+{
+    connect(&timer, &QTimer::timeout, this, &Monitor::refresh);
+
+    refresh();
+    timer.start(10000);
+}
+
+void Monitor::refresh()
+{
+    QSet<QString> newNames;
+    foreach(QNetworkInterface interface, QNetworkInterface::allInterfaces()) {
+        newNames.insert(interface.name());
+    }
+
+    foreach(QString name, newNames - oldNames) {
+        emit interfaceAdded(name);
+    }
+
+    foreach(QString name, oldNames - newNames) {
+        emit interfaceRemoved(name);
+    }
+
+    oldNames = newNames;
+}
