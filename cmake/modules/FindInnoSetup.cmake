e16d9c17e6104ba98c37377e2c8f310dba6f9631
Nathan Osman <nathan@quickmediasolutions.com>
2016-01-30 00:39:36 -0800

    Added missing include.

--- cmake/modules/FindInnoSetup.cmake
+++ cmake/modules/FindInnoSetup.cmake
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+include(FindPackageHandleStandardArgs)
+
 # Find the Inno Setup CLI compiler
 find_program(INNOSETUP_EXECUTABLE
     NAMES iscc
