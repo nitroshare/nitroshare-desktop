64c75fa700662b411ff0111134bd86d56e3bd540
Nathan Osman <nathan@quickmediasolutions.com>
2016-07-12 11:15:17 -0700

    Fixed icon rendering on OS X.

--- src/icon/trayicon.cpp
+++ src/icon/trayicon.cpp
@@ -22,8 +22,12 @@
  * IN THE SOFTWARE.
  **/
 
+#include <QApplication>
+#include <QDesktopWidget>
 #include <QIcon>
+#include <QPoint>
 #include <QRect>
+#include <QScreen>
 
 #include "../util/image.h"
 #include "trayicon.h"
@@ -31,18 +35,22 @@
 TrayIcon::TrayIcon()
 {
     mTrayIcon.setContextMenu(&mMenu);
+    mTrayIcon.setToolTip(tr("NitroShare"));
+    mTrayIcon.show();
 
     // KDE 5 requires that we load the SVG and render it to a pixmap - this is
     // also required to get retina-style icons in OS X
     QRect rect = mTrayIcon.geometry();
-    QPixmap *pixmap = Image::renderSvg(":/img/tray.svg", QSize(rect.width(), rect.height()));
+    int minSize = qMin(rect.width(), rect.height());
+    QScreen *screen = QApplication::screens().at(
+        QApplication::desktop()->screenNumber(QPoint(rect.x(), rect.y()))
+    );
+    minSize *= screen->devicePixelRatio();
+    QPixmap *pixmap = Image::renderSvg(":/img/tray.svg", QSize(minSize, minSize));
 
     // *Now* we can set the icon
     mTrayIcon.setIcon(QIcon(*pixmap));
     delete pixmap;
-
-    mTrayIcon.setToolTip(tr("NitroShare"));
-    mTrayIcon.show();
 }
 
 void TrayIcon::addAction(const QString &text, QObject *receiver, const char *member)
