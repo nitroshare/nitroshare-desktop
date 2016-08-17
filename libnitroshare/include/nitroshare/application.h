a018ea9dbd6fa11bcdfb615c333b0f94018e8beb
Nathan Osman <nathan@quickmediasolutions.com>
2016-08-16 19:46:27 -0700

    Add application class.

---
+++ libnitroshare/include/nitroshare/application.h
@@ -0,0 +1,73 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Nathan Osman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef LIBNITROSHARE_APPLICATION_H
+#define LIBNITROSHARE_APPLICATION_H
+
+#include <QObject>
+
+#include <nitroshare/devicemodel.h>
+#include <nitroshare/settings.h>
+
+#include "config.h"
+
+class NITROSHARE_EXPORT ApplicationPrivate;
+
+/**
+ * @brief Runtime application manager
+ *
+ * This class acts as a glorified container for most of the other classes in
+ * the library. By centralizing access to these instances, it becomes easier
+ * to write plugins.
+ */
+class NITROSHARE_EXPORT Application : public QObject
+{
+    Q_OBJECT
+
+public:
+
+    /**
+     * @brief Create a new application object
+     * @param parent QObject
+     */
+    explicit Application(QObject *parent = nullptr);
+
+    /**
+     * @brief Retrieve the global DeviceModel instance
+     * @return pointer to DeviceModel
+     */
+    DeviceModel *deviceModel() const;
+
+    /**
+     * @brief Retrieve the global settings instance
+     * @return pointer to Settings
+     */
+    Settings *settings() const;
+
+private:
+
+    ApplicationPrivate *const d;
+};
+
+#endif // LIBNITROSHARE_APPLICATION_H
