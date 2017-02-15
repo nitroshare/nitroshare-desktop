c85a861f2c1c0e7b628e9e8d694bcc743f7c3422
Nathan Osman <nathan@quickmediasolutions.com>
2017-02-14 23:06:21 -0800

    Add empty mDNS plugin.

---
+++ plugins/mdns/mdnsplugin.h
@@ -0,0 +1,41 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Nathan Osman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef MDNSPLUGIN_H
+#define MDNSPLUGIN_H
+
+#include <nitroshare/iplugin.h>
+
+class Q_DECL_EXPORT MdnsPlugin : public IPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID Plugin_iid FILE "mdns.json")
+
+public:
+
+    virtual void initialize(Application *application);
+    virtual void cleanup(Application *application);
+};
+
+#endif // MDNSPLUGIN_H
