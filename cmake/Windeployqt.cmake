60e13d9f1fe400c14903b64844e803b78dc129c7
Nathan Osman <nathan@quickmediasolutions.com>
2017-10-18 15:56:50 -0700

    Adjust windeployqt command to work with CPack.

--- cmake/Windeployqt.cmake
+++ cmake/Windeployqt.cmake
@@ -23,23 +23,14 @@
 find_package(Qt5Core REQUIRED)
 
 # Retrieve the absolute path to qmake and then use that path to find
-# the windeployqt and macdeployqt binaries
+# the windeployqt binary
 get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
 get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
-
 find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
-if(WINDEPLOYQT_EXECUTABLE)
-    message(STATUS "Found ${WINDEPLOYQT_EXECUTABLE}")
-endif()
 
-find_program(MACDEPLOYQT_EXECUTABLE macdeployqt HINTS "${_qt_bin_dir}")
-if(MACDEPLOYQT_EXECUTABLE)
-    message(STATUS "Found ${MACDEPLOYQT_EXECUTABLE}")
-endif()
-
-# Add commands that copy the required Qt files to the same directory as the
-# target after being built as well as including them in final installation
-function(windeployqt target)
+# Add commands that copy the Qt runtime to the target's output directory after
+# build and install the Qt runtime to the specified directory
+function(windeployqt target directory)
 
     # Run windeployqt immediately after build
     add_custom_command(TARGET ${target} POST_BUILD
@@ -53,9 +44,42 @@ function(windeployqt target)
         COMMENT "Deploying Qt..."
     )
 
-    # Install the files found in the destination directory
-    install(FILES "$<TARGET_FILE_DIR:${target}>/*"
-        DESTINATION "$<TARGET_PROPERTY:RUNTIME_OUTPUT_DIRECTORY>")
+    # install(CODE ...) doesn't support generator expressions, but
+    # file(GENERATE ...) does - store the path in a file
+    file(GENERATE OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${target}"
+        CONTENT "$<TARGET_FILE:${target}>"
+    )
+
+    # Before installation, run a series of commands that copy each of the Qt
+    # runtime files to the appropriate directory for installation
+    install(CODE
+        "
+        file(READ \"${CMAKE_CURRENT_BINARY_DIR}/${target}\" _file)
+        execute_process(
+            COMMAND \"${CMAKE_COMMAND}\" -E
+                env PATH=\"${_qt_bin_dir}\" \"${WINDEPLOYQT_EXECUTABLE}\"
+                    --dry-run
+                    --no-compiler-runtime
+                    --no-angle
+                    --no-opengl-sw
+                    --list mapping
+                    \${_file}
+            OUTPUT_VARIABLE _output
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        separate_arguments(_files WINDOWS_COMMAND \${_output})
+        while(_files)
+            list(GET _files 0 _src)
+            list(GET _files 1 _dest)
+            get_filename_component(_path \${_dest} DIRECTORY)
+            execute_process(
+                COMMAND \"${CMAKE_COMMAND}\" -E
+                    copy \${_src} \"\${CMAKE_INSTALL_PREFIX}/${directory}/\${_path}\"
+            )
+            list(REMOVE_AT _files 0 1)
+        endwhile()
+        "
+    )
 
     # windeployqt doesn't work correctly with the system runtime libraries,
     # so we fall back to one of CMake's own modules for copying them over
@@ -66,6 +90,8 @@ function(windeployqt target)
         message(WARNING "Deploying with MSVC 2015+ requires CMake 3.6+")
     endif()
 
+    # Install the runtime libraries and copy them to the output directory
+    # after build
     set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)
     include(InstallRequiredSystemLibraries)
     foreach(lib ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
@@ -76,17 +102,7 @@ function(windeployqt target)
             COMMENT "Copying ${filename}..."
         )
     endforeach()
+
 endfunction()
 
-# Add commands that copy the required Qt files to the application bundle
-# represented by the target.
-function(macdeployqt target)
-    add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND "${MACDEPLOYQT_EXECUTABLE}"
-            \"$<TARGET_FILE_DIR:${target}>/../..\"
-            -always-overwrite
-        COMMENT "Deploying Qt..."
-    )
-endfunction()
-
-mark_as_advanced(WINDEPLOYQT_EXECUTABLE MACDEPLOYQT_EXECUTABLE)
+mark_as_advanced(WINDEPLOYQT_EXECUTABLE)
