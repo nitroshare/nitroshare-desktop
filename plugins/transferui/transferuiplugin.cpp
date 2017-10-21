19988fbdee086a0409ced762b8206aa03eb44190
Nathan Osman <nathan@quickmediasolutions.com>
2017-10-20 23:59:36 -0700

    Add transferui plugin.

---
+++ plugins/transferui/transferuiplugin.cpp
@@ -0,0 +1,41 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Nathan Osman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <nitroshare/actionregistry.h>
+#include <nitroshare/application.h>
+
+#include "transfersaction.h"
+#include "transferuiplugin.h"
+
+void TransferUiPlugin::initialize(Application *application)
+{
+    mTransfersAction = new TransfersAction;
+    application->actionRegistry()->add(mTransfersAction);
+}
+
+void TransferUiPlugin::cleanup(Application *application)
+{
+    application->actionRegistry()->remove(mTransfersAction);
+    delete mTransfersAction;
+}
