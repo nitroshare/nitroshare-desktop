edf73404d699a0b9d3519aaf23163a8f28684dd4
Nathan Osman <nathan@quickmediasolutions.com>
2017-11-06 16:48:06 -0800

    Rewrite nmh to use ApiUtil.

--- nmh/main.cpp
+++ nmh/main.cpp
@@ -25,15 +25,12 @@
 #include <cstdio>
 
 #include <QCoreApplication>
-#include <QDir>
-#include <QFile>
 #include <QJsonDocument>
 #include <QJsonObject>
-#include <QJsonParseError>
-#include <QNetworkAccessManager>
-#include <QNetworkReply>
-#include <QNetworkRequest>
-#include <QUrl>
+#include <QJsonValue>
+#include <QVariantMap>
+
+#include <nitroshare/apiutil.h>
 
 /**
  * @brief Read the next message from STDIN
@@ -56,62 +53,21 @@ bool readData(QByteArray &data)
 /**
  * @brief Parse data in a message
  * @param data message content
- * @param name storage for the action name
- * @param parameters storage for the action parameters
+ * @param action storage for the action
+ * @param params storage for the action parameters
  * @param error storage for an error message
  * @return true if no errors occurred
  */
-bool parseData(const QByteArray &data, QString &name, QByteArray &parameters, QString &error)
+bool parseData(const QByteArray &data, QString &action, QVariantMap &params, QString &error)
 {
     QJsonObject object = QJsonDocument::fromJson(data).object();
-    name = object.value("name").toString();
-    QJsonObject parametersObject = object.value("parameters").toObject();
-    parameters = QJsonDocument(parametersObject).toJson(QJsonDocument::Compact);
-    if (name.isNull()) {
+    action = object.value("name").toString();
+    params = object.value("parameters").toObject().toVariantMap();
+    if (action.isNull()) {
         error = QObject::tr("invalid request received");
         return false;
     }
     return true;
-}
-
-/**
- * @brief Attempt to find NitroShare running under the same account
- * @param port storage for the local port to connect to
- * @param token storage for the auth token
- * @param error storage for an error message
- * @return true if NitroShare was found running
- */
-bool findNitroShare(quint16 &port, QString &token, QString &error)
-{
-    QFile file(QDir::home().absoluteFilePath(".NitroShare"));
-    if (!file.open(QIODevice::ReadOnly)) {
-        error = QObject::tr("NitroShare does not appear to be running");
-        return false;
-    }
-    QJsonObject object = QJsonDocument::fromJson(file.readAll()).object();
-    port = object.value("port").toInt();
-    token = object.value("token").toString();
-    if (!port || token.isNull()) {
-        error = QObject::tr("unable to read NitroShare configuration");
-        return false;
-    }
-    return true;
-}
-
-/**
- * @brief Create a request to the NitroShare client
- * @param port NitroShare port
- * @param name action name
- * @param token auth token
- * @return newly created request
- */
-QNetworkRequest createRequest(quint16 port, const QString &name, const QString &token)
-{
-    QUrl url(QString("http://localhost:%1/api/%2").arg(port).arg(name));
-    QNetworkRequest request(url);
-    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
-    request.setRawHeader("X-Auth-Token", token.toUtf8());
-    return request;
 }
 
 /**
@@ -140,59 +96,39 @@ void writeError(const QString &message)
 int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
-    QNetworkAccessManager manager;
+    Q_UNUSED(app)
 
     forever {
 
-        // Read the next message from STDIN
-        QByteArray data;
-        if (!readData(data)) {
-            break;
-        }
-
-        QString error;
-
-        // Parse the data
-        QString name;
-        QByteArray parameters;
-        if (!parseData(data, name, parameters, error)) {
-            writeError(error);
-            continue;
-        }
-
-        // Find NitroShare (if it is running)
-        quint16 port;
-        QString token;
-        if (!findNitroShare(port, token, error)) {
-            writeError(error);
-            continue;
-        }
-
-        // Send the request and wait for completion
-        QNetworkReply *reply = manager.post(createRequest(port, name, token), parameters);
-
-        // Process the request when it completes
-        QObject::connect(reply, &QNetworkReply::finished, [reply]() {
-            reply->deleteLater();
-            if (reply->error() != QNetworkReply::NoError ||
-                    reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt() != 200) {
-                writeError(QObject::tr("error communicating with NitroShare"));
-                return;
+            // Read the next message from STDIN
+            QByteArray data;
+            if (!readData(data)) {
+                break;
             }
-            QJsonParseError jsonError;
-            QJsonDocument document = QJsonDocument::fromJson(reply->readAll(), &jsonError);
-            if (jsonError.error != QJsonParseError::NoError) {
-                writeError(QObject::tr("invalid response received"));
-                return;
+
+            QString error;
+
+            // Parse the data
+            QString action;
+            QVariantMap params;
+            if (!parseData(data, action, params, error)) {
+                writeError(error);
+                continue;
             }
-            writeData(document.toJson(QJsonDocument::Compact));
-        });
 
-        // Quit the event loop when the reply is received
-        QObject::connect(reply, &QNetworkReply::finished, &app, &QCoreApplication::quit);
+            // Send the request
+            QVariant returnVal;
+            if (ApiUtil::sendRequest(action, params, returnVal, &error)) {
 
-        // Run the event loop
-        app.exec();
+                // Send a reply with the JSON
+                QJsonObject object = QJsonObject{
+                    { "return", QJsonValue::fromVariant(returnVal) }
+                };
+                writeData(QJsonDocument(object).toJson());
+
+            } else {
+                writeError(error);
+            }
     }
 
     return 0;
