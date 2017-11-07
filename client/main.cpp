861637b47312a6ec304412c79f179518587360d2
Nathan Osman <nathan@quickmediasolutions.com>
2017-11-06 20:16:13 -0800

    Complete initial version of client.

--- client/main.cpp
+++ client/main.cpp
@@ -22,13 +22,83 @@
  * IN THE SOFTWARE.
  */
 
+#include <iostream>
+
 #include <QCoreApplication>
+#include <QObject>
+#include <QVariantMap>
+
+#include <nitroshare/apiutil.h>
+
+/**
+ * @brief Print usage information
+ */
+void printUsage()
+{
+    std::cerr << "NitroShare CLI client\n"
+              << "Version: " << NITROSHARE_VERSION << "\n\n"
+              << "Usage: " << QCoreApplication::arguments().at(0).toUtf8().constData()
+              << " ACTION [PARAMS]\n\n";
+}
+
+/**
+ * @brief Parse the command line arguments
+ * @param action storage for the action
+ * @param params storage for the action parameters
+ * @param error description of error
+ * @return true if the arguments were parsed
+ */
+bool parseArguments(QString &action, QVariantMap &params, QString &error)
+{
+    QStringList args = QCoreApplication::arguments();
+
+    // Ensure at least two parameters were supplied
+    if (args.count() < 2) {
+        error = QObject::tr("action missing");
+        return false;
+    }
+
+    action = args.at(1);
+
+    // Parse the parameters
+    for (auto i = args.constBegin() + 2; i != args.constEnd(); ++i) {
+        if (!(*i).startsWith("--")) {
+            error = QObject::tr("parameter must start with \"--\"");
+            return false;
+        }
+        QString paramName = *(i++);
+        if (i == args.constEnd()) {
+            error = QObject::tr("missing value for \"%1\"").arg(paramName);
+            return false;
+        }
+        params.insert(paramName, *i);
+    }
+
+    return true;
+}
 
 int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
+    Q_UNUSED(app)
 
-    //...
+    QString action;
+    QVariantMap params;
+    QString error;
 
-    return app.exec();
+    // Parse the arguments
+    if (!parseArguments(action, params, error)) {
+        printUsage();
+        std::cerr << "Error: " << error.toUtf8().constData() << std::endl;
+        return 1;
+    }
+
+    // Send the request
+    QVariant returnVal;
+    if (!ApiUtil::sendRequest(action, params, returnVal, &error)) {
+        std::cerr << "Error: " << error.toUtf8().constData() << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
