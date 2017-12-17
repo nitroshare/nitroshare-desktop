4079fcb59ec0f82d2ee5e9b837406973a0af185e
Nathan Osman <nathan@quickmediasolutions.com>
2017-12-16 16:59:53 -0800

    Reorganize windeployqt calls.

--- cmake/SharedLibrary.cmake
+++ cmake/SharedLibrary.cmake
@@ -25,10 +25,8 @@
 function(copy_lib_win src dest)
     add_custom_command(TARGET ${dest} POST_BUILD
         COMMAND "${CMAKE_COMMAND}" -E
-            copy_if_different \"$<TARGET_FILE:${src}>\" \"${CMAKE_RUNTIME_OUTPUT_DIRECTORY}\"
-    )
-    install(FILES "$<TARGET_FILE:${src}>"
-        DESTINATION ${INSTALL_BIN_PATH}
+            copy_if_different \"$<TARGET_FILE:${src}>\" \"$<TARGET_FILE_DIR:${dest}>\"
+        COMMENT "Copying ${src} to ${dest}..."
     )
 endfunction()
 
