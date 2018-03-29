cc1ee1bb77d004b654c05a0e3356ce600f0e161e
Nathan Osman <nathan@quickmediasolutions.com>
2018-03-28 23:45:10 -0700

    Corrections to the installation script for Windows.

--- scripts/windows.bat
+++ scripts/windows.bat
@@ -1,16 +1,19 @@
 @ECHO OFF
 
-ECHO "Building installer for Windows"
-
-REM Begin by configuring the project in the current directory
+REM Configure the application in the current directory
 cmake ^
     -G"NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_INSTALL_PREFIX=%cd%/out ^
-    %~dp0\..
+    -DCMAKE_INSTALL_PREFIX=out ^
+    %~dp0\.. || exit /b
 
-REM Build the entire project
-nmake
+REM Build and install the application
+nmake || exit /b
+nmake install || exit /b
 
-REM Install the project to the out directory
-nmake install
+REM Run windeployqt on nitroshare-ui
+windeployqt ^
+    --verbose 0 ^
+    --no-angle ^
+    --no-opengl-sw ^
+    out/bin/nitroshare-ui || exit /b
